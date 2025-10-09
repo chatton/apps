53c2014fb600cfbca52c2af9f30aad8ba3d7e7c6
chatton <github.qpeyb@simplelogin.fr>
2025-10-09 12:56:32 +0100

    chore: bump abci version

--- evolve/template/constants.go
+++ evolve/template/constants.go
@@ -10,7 +10,7 @@ const (
 
 const (
 	EvABCIPackage = "github.com/evstack/ev-abci"
-	EvABCIVersion = "v0.0.0-20251009114243-528f47e03ae2"
+	EvABCIVersion = "v0.0.0-20251009115557-bf2235abbb14"
 
 	EvNodePackage = "github.com/evstack/ev-node"
 	EvNodeVersion = "v1.0.0-beta.5"
